6df3e68d0b5b0896ec9c4c75c79d44147157b680
agamswaroop619 <agam.swaroop619@gmail.com>
2024-06-17 16:29:58 +0530

    add: Home Route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,44 +1,13 @@
+"use client";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 import Header from "./components/header";
 import Footer from "./components/footer";
-export default function Home() {
-  return (
-    <div style={{ fontFamily: "Arial, sans-serif" }}>
-      <Header />
-      <section style={{ marginBottom: "20px" }}>
-        <h2>System Certification Support</h2>
-        <ul>
-          <li>Quality Management System ISO 9000/ IATF 16949</li>
-          <li>Occupational Health and Safety Management System ISO 45001</li>
-          <li>
-            Environment and Energy Management System ISO 14000 and ISO 5000
-          </li>
-          <li>Internal Audit and Guidance as per QMS/ OHSMS/ EMS</li>
-          <li>
-            Online Training for Standard (Specially on weekends for enhancing
-            knowledge of working professionals)
-          </li>
-        </ul>
-      </section>
 
-      <section>
-        <h2>Support – Continual Quality Improvement</h2>
-        <ul>
-          <li>
-            Development of APQP and PPAP documents, specially:
-            <ul>
-              <li>Process Flow Chart</li>
-              <li>FMEA</li>
-              <li>Control Plan</li>
-            </ul>
-          </li>
-          <li>
-            Station Specific Pictorial Work Instruction / Standard Work Method
-            (Both Hindi and English language)
-          </li>
-          <li>Training and Support for Problem Solving tool</li>
-        </ul>
-      </section>
-      <Footer />
-    </div>
-  );
+export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/home");
+  }, [router]);
 }
