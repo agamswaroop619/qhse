18fa1a4bdde8b0be24c95193ea9188035f3ecc03
agamswaroop619 <agam.swaroop619@gmail.com>
2024-06-17 17:15:18 +0530

    fix: NavBar Styling

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -2,93 +2,91 @@
 import React, { useState } from "react";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
-import Menu from "@mui/material/Menu";
-import MenuItem from "@mui/material/MenuItem";
 import Typography from "@mui/material/Typography";
 import { useTheme } from "@mui/material/styles";
 import useMediaQuery from "@mui/material/useMediaQuery";
-import MenuIcon from "@mui/icons-material/Menu";
 import Link from "next/link";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
+import BottomNavigation from "@mui/material/BottomNavigation";
+import BottomNavigationAction from "@mui/material/BottomNavigationAction";
+import HomeIcon from "@mui/icons-material/Home";
+import BuildIcon from "@mui/icons-material/Build";
+import InfoIcon from "@mui/icons-material/Info";
 
 export default function Navbar() {
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
-  const [anchorEl, setAnchorEl] = useState(null);
   const [value, setValue] = useState(0);
-
-  const handleMenuOpen = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleMenuClose = () => {
-    setAnchorEl(null);
-  };
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
   return (
-    <AppBar position="static">
-      <Toolbar>
-        <Typography variant="h6" style={{ flexGrow: 1 }}>
-          My Website
-        </Typography>
-        {isMobile ? (
-          <>
-            <IconButton
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              onClick={handleMenuOpen}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              anchorEl={anchorEl}
-              open={Boolean(anchorEl)}
-              onClose={handleMenuClose}
-            >
-              <MenuItem onClick={handleMenuClose}>
-                <Link href="/home" passHref>
-                  <Typography component="a" color="inherit">
-                    Home
-                  </Typography>
-                </Link>
-              </MenuItem>
-              <MenuItem onClick={handleMenuClose}>
-                <Link href="/solutions" passHref>
-                  <Typography component="a" color="inherit">
-                    Solutions
-                  </Typography>
-                </Link>
-              </MenuItem>
-              <MenuItem onClick={handleMenuClose}>
-                <Link href="/about" passHref>
-                  <Typography component="a" color="inherit">
-                    About
-                  </Typography>
-                </Link>
-              </MenuItem>
-            </Menu>
-          </>
-        ) : (
-          <Tabs
-            value={value}
-            onChange={handleChange}
-            aria-label="navigation tabs"
-            indicatorColor="secondary"
-            textColor="inherit"
+    <div style={{ display: "flex", flexDirection: "column", height: "100vh" }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography
+            variant="h6"
+            style={{
+              flexGrow: 1,
+              textAlign: isMobile ? "center" : "left",
+            }}
           >
-            <Tab label="Home" component={Link} href="/home" />
-            <Tab label="Solutions" component={Link} href="/solutions" />
-            <Tab label="About" component={Link} href="/about" />
-          </Tabs>
-        )}
-      </Toolbar>
-    </AppBar>
+            My Website
+          </Typography>
+          {!isMobile && (
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              aria-label="navigation tabs"
+              indicatorColor="secondary"
+              textColor="inherit"
+            >
+              <Tab label="Home" component={Link} href="/home" />
+              <Tab label="Solutions" component={Link} href="/solutions" />
+              <Tab label="About" component={Link} href="/about" />
+            </Tabs>
+          )}
+        </Toolbar>
+      </AppBar>
+      {isMobile && (
+        <BottomNavigation
+          value={value}
+          onChange={handleChange}
+          showLabels
+          style={{
+            width: "100%",
+            position: "fixed",
+            bottom: 0,
+            backgroundColor: theme.palette.primary.main,
+            color: theme.palette.primary.contrastText,
+          }}
+        >
+          <BottomNavigationAction
+            label="Home"
+            icon={<HomeIcon />}
+            component={Link}
+            href="/home"
+            style={{ color: theme.palette.primary.contrastText }}
+          />
+          <BottomNavigationAction
+            label="Solutions"
+            icon={<BuildIcon />}
+            component={Link}
+            href="/solutions"
+            style={{ color: theme.palette.primary.contrastText }}
+          />
+          <BottomNavigationAction
+            label="About"
+            icon={<InfoIcon />}
+            component={Link}
+            href="/about"
+            style={{ color: theme.palette.primary.contrastText }}
+          />
+        </BottomNavigation>
+      )}
+    </div>
   );
 }
